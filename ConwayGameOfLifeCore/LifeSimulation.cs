5501624bdb5c67ca5b8e7a7ab546cb542e08cdb9
Dennis <dennis.ostermann@ise.de>
2025-04-22 12:51:46 +0200

    Pattern Matching

--- ConwayGameOfLifeCore/LifeSimulation.cs
+++ ConwayGameOfLifeCore/LifeSimulation.cs
@@ -5,20 +5,20 @@ using System.Threading.Tasks;
 
 namespace ConwayGameOfLifeCore
 {
+    /// <summary>
+    /// Primary constructors
+    /// </summary>
+    /// <param name="sizex"></param>
+    /// <param name="sizey"></param>
     public class LifeSimulation(int sizex, int sizey)
     {
         private bool[,] world;
 
-        private readonly List<(int offsetx, int offsety)> neighbors =
+        private readonly (int offsetx, int offsety)[] neighbors =
         [
-            (-1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1),
-            (-1, -1)
+           (-1, 1), (0, 1),   (1, 1),   
+           (-1, 0),      /*CENTER*/         (1, 0), 
+           (-1, -1), (0, -1), (1, -1)
         ];
 
         public int SizeX { get; } = sizex;
@@ -48,38 +48,42 @@ namespace ConwayGameOfLifeCore
 
             return nextGeneration;
         }
+
         private async IAsyncEnumerable<(int x, int y, bool shouldLive)> GetUpdatedDatapoints()
+        {
+            await foreach (var (x, y, isAlive, aliveNeighbors) in GetCellsWithNeighborCount())
+            {
+                yield return (x, y, isAlive switch
+                {
+                    true when aliveNeighbors is 2 or 3 => true,   // Cell stays alive
+                    false when aliveNeighbors == 3 => true,      // Cell becomes alive (zombification)
+                    _ => false                                   // Cell dies
+                });
+            }
+        }
+
+        private async IAsyncEnumerable<(int x, int y, bool isAlive, int aliveNeighbors)> GetCellsWithNeighborCount()
         {
             for (int x = 0; x < SizeX; x++)
             {
                 for (int y = 0; y < SizeY; y++)
                 {
-                    bool shouldLive = false;
-                    await Task.Run(
-                      () =>
-                        {
-                            int numberOfAliveNeighbors = neighbors.Select(tuple => (x: tuple.offsetx + x, y: tuple.offsety + y)).Select(IsNeighborAlive).Sum();
-                            bool isAlive = world[x, y];
+                    bool isAlive = world[x, y];
+                    int aliveNeighbors = await Task.Run(() =>
+                        neighbors
+                            .Select(tuple => (x: tuple.offsetx + x, y: tuple.offsety + y))
+                            .Count(IsNeighborAlive)
+                    );
 
-                            if (isAlive && (numberOfAliveNeighbors == 2 || numberOfAliveNeighbors == 3))
-                            {
-                                shouldLive = true;
-                            }
-                            else if (!isAlive && numberOfAliveNeighbors == 3) //zombification
-                            {
-                                shouldLive = true;
-                            }
-                        });
-
-                    yield return (x, y, shouldLive);
+                    yield return (x, y, isAlive, aliveNeighbors);
                 }
             }
         }
 
-        private int IsNeighborAlive((int x, int y) neighbor)
+        private bool IsNeighborAlive((int x, int y) neighbor)
         {
             bool outOfBounds = neighbor.x < 0 || neighbor.x >= SizeX | neighbor.y < 0 || neighbor.y >= SizeY;
-            return Convert.ToInt32(!outOfBounds && world[neighbor.x, neighbor.y]);
+            return !outOfBounds && world[neighbor.x, neighbor.y];
         }
 
         public void Randomize()
