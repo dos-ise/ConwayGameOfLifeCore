8f33bd377d010378c56b0d47b68788b1e3f6ed9b
Dennis <dennis.ostermann@ise.de>
2025-04-22 12:12:05 +0200

    Added support for game board which are not equally squared

--- ConwayGameOfLifeCore/LifeSimulation.cs
+++ ConwayGameOfLifeCore/LifeSimulation.cs
@@ -1,97 +1,98 @@
-﻿namespace ConwayGameOfLifeCore
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConwayGameOfLifeCore
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Threading.Tasks;
-
-  public class LifeSimulation
-  {
-    private bool[,] world;
-    private readonly List<(int offsetx, int offsety)> neighbors = new List<(int offsetx, int offsety)>()
-                                                                    {
-                                                                      (-1, 0),
-                                                                      (-1, 1),
-                                                                      (0, 1),
-                                                                      (1, 1),
-                                                                      (1, 0),
-                                                                      (1, -1),
-                                                                      (0, -1),
-                                                                      (-1, -1),
-                                                                    };
-    public LifeSimulation(int size) => Size = size;
-
-    public int Size { get; }
-
-    public int Generation { get; private set; }
-
-    public bool this[int x, int y]
+    public class LifeSimulation(int sizex, int sizey)
     {
-      get => world[x, y];
-      set => world[x, y] = value;
-    }
+        private bool[,] world;
 
-    public async Task Update()
-    {
-      world = await ProcessGeneration();
-      Generation++;
-    }
+        private readonly List<(int offsetx, int offsety)> neighbors =
+        [
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+            (1, 0),
+            (1, -1),
+            (0, -1),
+            (-1, -1)
+        ];
 
-    private async Task<bool[,]> ProcessGeneration()
-    {
-      bool[,] nextGeneration = new bool[Size, Size];
-      await foreach (var dataPoint in GetUpdatedDatapoints())
-      {
-        nextGeneration[dataPoint.x, dataPoint.y] = dataPoint.shouldLive;
-      }
+        public int SizeX { get; } = sizex;
+        public int SizeY { get; } = sizey;
 
-      return nextGeneration;
-    }
-    private async IAsyncEnumerable<(int x, int y, bool shouldLive)> GetUpdatedDatapoints()
-    {
-      for (int x = 0; x < Size; x++)
-      {
-        for (int y = 0; y < Size; y++)
+        public int Generation { get; private set; }
+
+        public bool this[int x, int y]
         {
-          bool shouldLive = false;
-          await Task.Run(
-            () =>
-              {
-                int numberOfAliveNeighbors = neighbors.Select(tuple => (x: tuple.offsetx + x, y: tuple.offsety + y)).Select(IsNeighborAlive).Sum();
-                bool isAlive = world[x, y];
-
-                if (isAlive && (numberOfAliveNeighbors == 2 || numberOfAliveNeighbors == 3))
-                {
-                  shouldLive = true;
-                }
-                else if (!isAlive && numberOfAliveNeighbors == 3) //zombification
-                {
-                  shouldLive = true;
-                }
-              });
-
-          yield return (x, y, shouldLive);
+            get => world[x, y];
+            set => world[x, y] = value;
         }
-      }
-    }
 
-    private int IsNeighborAlive((int x, int y) neighbor)
-    {
-      bool outOfBounds = neighbor.x < 0 || neighbor.x >= Size | neighbor.y < 0 || neighbor.y >= Size;
-      return Convert.ToInt32(!outOfBounds && world[neighbor.x, neighbor.y]);
-    }
-
-    public void Randomize()
-    {
-      var random = new Random();
-      world = new bool[Size, Size];
-      for (int x = 0; x < Size; x++)
-      {
-        for (int y = 0; y < Size; y++)
+        public async Task Update()
         {
-          world[x, y] = random.Next(2) != 0;
+            world = await ProcessGeneration();
+            Generation++;
         }
-      }
+
+        private async Task<bool[,]> ProcessGeneration()
+        {
+            bool[,] nextGeneration = new bool[SizeX, SizeY];
+            await foreach (var dataPoint in GetUpdatedDatapoints())
+            {
+                nextGeneration[dataPoint.x, dataPoint.y] = dataPoint.shouldLive;
+            }
+
+            return nextGeneration;
+        }
+        private async IAsyncEnumerable<(int x, int y, bool shouldLive)> GetUpdatedDatapoints()
+        {
+            for (int x = 0; x < SizeX; x++)
+            {
+                for (int y = 0; y < SizeY; y++)
+                {
+                    bool shouldLive = false;
+                    await Task.Run(
+                      () =>
+                        {
+                            int numberOfAliveNeighbors = neighbors.Select(tuple => (x: tuple.offsetx + x, y: tuple.offsety + y)).Select(IsNeighborAlive).Sum();
+                            bool isAlive = world[x, y];
+
+                            if (isAlive && (numberOfAliveNeighbors == 2 || numberOfAliveNeighbors == 3))
+                            {
+                                shouldLive = true;
+                            }
+                            else if (!isAlive && numberOfAliveNeighbors == 3) //zombification
+                            {
+                                shouldLive = true;
+                            }
+                        });
+
+                    yield return (x, y, shouldLive);
+                }
+            }
+        }
+
+        private int IsNeighborAlive((int x, int y) neighbor)
+        {
+            bool outOfBounds = neighbor.x < 0 || neighbor.x >= SizeX | neighbor.y < 0 || neighbor.y >= SizeY;
+            return Convert.ToInt32(!outOfBounds && world[neighbor.x, neighbor.y]);
+        }
+
+        public void Randomize()
+        {
+            var random = new Random();
+            world = new bool[SizeX, SizeY];
+            for (int x = 0; x < SizeX; x++)
+            {
+                for (int y = 0; y < SizeY; y++)
+                {
+                    world[x, y] = random.Next(2) != 0;
+                }
+            }
+        }
     }
-  }
 }
